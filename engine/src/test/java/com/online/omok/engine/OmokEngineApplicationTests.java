5888e526182cc207c3000229ac88dc46fc0ea851
hy <lookhkh37@gmail.com>
2025-04-18 00:31:23 +0900

    모노 레포 기본 세팅

---
+++ engine/src/test/java/com/online/omok/engine/OmokEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.omok.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmokEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
