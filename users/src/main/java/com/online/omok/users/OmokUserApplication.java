0f88f3fdb06014b2db5b240f7d50d208df6465fc
hyunil-cho <jackandjills@naver.com>
2025-04-18 15:09:34 +0900

    user 관리 서브프로젝트 추가

---
+++ users/src/main/java/com/online/omok/users/OmokUserApplication.java
@@ -0,0 +1,14 @@
+package com.online.omok.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class OmokUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmokUserApplication.class, args);
+	}
+
+}
